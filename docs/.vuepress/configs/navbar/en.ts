564e29aa05dc62b16dca264ed9403c4ec225ec6d
Hein Thu <thu105@mail.chapman.edu>
2022-05-04 14:49:32 -0700

    add: navbar config

---
+++ docs/.vuepress/configs/navbar/en.ts
@@ -0,0 +1,20 @@
+import type { NavbarConfig } from "vuepress-theme-gungnir";
+
+export const en: NavbarConfig = [
+  {
+    text: 'Home',
+    link: '/',
+  },
+  {
+    text: 'Projects',
+    link: '/tags/Project/',
+  },
+  {
+    text: 'Blog',
+    link: '/tags/',
+  },
+  {
+    text: 'About',
+    link: '/about/promoes.md',
+  }
+];
