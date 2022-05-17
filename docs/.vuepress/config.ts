520ca56eeed5bd6662c29c0acdbf9995470a1810
Hein Moe Thu <thu105@mail.chapman.edu>
2022-05-17 11:07:01 -0700

    add: hero photos

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -87,19 +87,52 @@ export default defineUserConfig<GungnirThemeOptions>({
 
     // Hero images on home page
     homeHeaderImages: [
-      // image 1
       {
         "path": "/img/home/hero/1.jpg",
         "mask": "rgba(0, 0, 0, .1)"
       },
-      // image 2
       {
-        "path": "/img/home/hero/2.jpg",
-        "mask": "rgb(0, 0, 0, .1)"
-      }
+        "path": "https://lh3.googleusercontent.com/KWN0778-X4ldnbGih93-2zYMYh8IuwKhpb_MBOAiB3mv4St5HWA0u91P4HFV4m1PhoexwyGZxoIIAM26GaXxiL3R03ygUjX6vpWXzKFhv0aRmfohjGkv8O2tT06ABKbtrkzqhBXCkGc=w2400",
+        "mask": "rgba(0, 0, 0, .2)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/MHK0074UOumf1x25qO5HX9-hbDr-miQCiFwxGZxcXE9Wzlmi--KjGNMYkg-1SV_AjKJCpKopBjCtgKA4iC1zZbhfeaDfjPLY1CSTfWACiSBqtINrU0ir1M8TpOjZl9eX6H_RY4Lo9ww=w2400",
+        "mask": "rgba(0, 0, 0, .2)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/PXFRUVpgVEQiIzq3vaFsl9A4pJH3Zciek7Z6Qt7z-mrhtOGC_RbF6cYVsdnsKdz-TmMeJPUJrbt793SXdLPqHH6k0oCSwnutOgy4YydFi5zmh-3bnAse0bc_BrHBRMBfIFd2WFE0URI=w2400",
+        "mask": "rgba(0, 0, 0, .4)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/cahFeZbM9GBFl-cC610c-iYVXefYxJvgmQRETZvUMl8SLIGHEsH5Ma6EEFkbJqAupVTWTHQNTa5Z0TltAkRn4gasIMYigZ4_f7Hcb9UpC_mTvkevkR2eCoIRDdaH1ih13UFCMkm2yO8=w2400",
+        "mask": "rgba(0, 0, 0, .1)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/BzSpAumzPaK-rpm1nglWTHabXET4UYf6JWHKYE9DX2Nt3HiWc7MF0vAiUwUt5ofvhjlAD6-vMhobTFLOZljFUtcf8vPWnj4Pi8ObM47Ea8rgS4vKxkJvoehmCNptKLtkxaR1qqtcT9g=w2400",
+        "mask": "rgba(0, 0, 0, .4)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/2j-257CuYtIFwy1B8OuXH66hfbJKa3lOMpMEQm8IwPXinMldSEqDwx5BuOCuZnvnI75WIv2vJNfQg5FNYZNmr8hPAk8cnyhYqDNcgu7poEYBbV4I0bAWafkco1g8g96zD-pEEvvgsbg=w2400",
+        "mask": "rgba(0, 0, 0, .5)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/F3wUdvW7fx-6K-H1rxKHP6BkMacHH8Eec7OgeEHmIFCP_tcBeuB9rMzuNUhaL-46GQxPgZOGoYl7vVth4il1gxjoPAUHDtpyYYcYD7pMHvFaFHa0WUBzCPhhAzpb14rRWzmohksEksw=w2400",
+        "mask": "rgba(0, 0, 0, .5)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/wCBUesvWoMYUxVywp-Z-RDQPeTgQb7RyxFtH9hl2nfIT_yBlplCoXVtfsNyvT-bS1yOZ1eTWpIfFRQNz6zN6oczWaDW38OLnAmtYrM06UThHYaxS4ihG6QWtMCaya5Rv8H88orDoOR4=w2400",
+        "mask": "rgba(0, 0, 0, .5)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/FHdm4uewf0N5cx3k_swP9Ue6TKgicB-MWJVz96Q0dlqWtPJixzzneqjGQk22YEIQXoN5cj9p5U_wnDmWmXDRa4VAWyNQEBKCVcMxBVj946JXGbkzD1o1KHfpxOWvdYmF-w680m8TMiU=w2400",
+        "mask": "rgba(0, 0, 0, .5)"
+      },
+      {
+        "path": "https://lh3.googleusercontent.com/CWt8Y4dW_QSLLzFECk3l83mrSbpGDRM7xKZVFqgLurLYYXBtHG5wS5pZCivRJwCuGQroIcC1gv80X0RJ1NNXbnQ1wb7X1WRJzzs4isNG8LKdL8RV7DZu8ZYRESlPl8xA2YwPfx8G3xM=w2400",
+        "mask": "rgba(0, 0, 0, .5)"
+      },
     ],
 
-    // additional pages
     pages: {
       // posts page
       tags: {
